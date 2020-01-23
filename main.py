a1472ac2b18ca81e8b4008f6c922a1aac3b3f21f
kashyl <staviladeian@gmail.com>
2020-01-23 15:21:10 +0000

    Added cloud MongoDB connection and data storing, more...
    Created a cloud MongoDB cluster and made loop store all data as documents in the regional_weather collection.
    
    Take into account authentication is required, so you need an user account (ask me). Once you have one, add it to environment variables. Installation of MongoDB on the OS and PyCharm driver (if applies) is also required. MongoDB Compass is an amazing tool for visualizing the cluster as well as work with it (great interface compared to raw shell commands)
    
    Todo: Refine data storing in DB (bucket patterns).

--- main.py
+++ main.py
@@ -3,34 +3,56 @@ import os  # Miscellaneous operating system interfaces, https://docs.python.org/
 from datetime import datetime
 # pprint = pretty print with indentation, good for JSON files so not everything is on the same line
 from pprint import pprint  # so we can do pprint(stuff) instead of pprint.pprint(stuff)
+from datetime import datetime
 import asyncio
 import aiohttp  # fetch data from urls asynchronously (compared to import requests which is sync)
 import json
 import urllib  # converts strings to url format (needed in case of special characters)
 import urllib.parse
+import pymongo as pymongo
 
-weatherKey = os.environ['WEATHER_API_KEY']  # Fetch OpenWeatherMap API key from environmental variables
-location = "Hong Kong"
+"""Get environment variables"""
+dbUser = os.environ['DB_USER']
+dbPass = os.environ['DB_PASS']
+dbPassURL = urllib.parse.quote(dbPass)
+weatherKey = os.environ['WEATHER_API_KEY']  # Fetch OpenWeatherMap API key from environment variables
+"""End environment variables"""
+
+"""Connect to MongoDB"""
+client = pymongo.MongoClient(f'mongodb+srv://{dbUser}:{dbPassURL}'
+                             f'@issf2020hk-la5xb.gcp.mongodb.net/test?retryWrites=true&w=majority')
+db = client["mydb"]
+users_col = db["users"]
+weather_col = db["regional_weather"]
+print(db.list_collection_names())
 
 
-async def fetch_weather_data():
+async def regional_weather_data():
     """Fetches the data from the openweathermap.org api asynchronously in weather_resp"""
     while True:  # so the loop continues after one run; use this as long as async call is performed inside
+        location = "Hong Kong"
         async with aiohttp.ClientSession() as session:
             async with session.get(f'http://api.openweathermap.org/data/2.5/weather?q={location}&APPID={weatherKey}') \
                     as weather_req:
-                weather_resp = json.loads(await weather_req.text())
-                pprint(weather_resp)
-                # converts the default Kelvin to Celsius °C
-                celsius = round(weather_resp['main']['temp'] - 273.15, 2)
-        await asyncio.sleep(300)  # the time between API calls
+                weather_resp = json.loads(await weather_req.text())  # Converts the request data into JSON format
+                celsius = round(weather_resp['main']['temp'] - 273.15, 2)  # converts the default Kelvin to Celsius °C
+                # pprint(weather_resp)  # for debugging
+                weather_col.insert_one(weather_resp)  # Store to database
+                print("Stored regional data @", end=" ")  # status message
+                print(datetime.now(), end=" ")  # print timestamp
+                print(f'- {weather_resp["name"]}: {celsius}°C, {weather_resp["weather"][0]["description"]} ')
 
+        await asyncio.sleep(30)  # the time between API calls
+
+
+"""Start of asyncio event loop"""
 loop = asyncio.get_event_loop()
 try:
-    asyncio.ensure_future(fetch_weather_data())
+    asyncio.ensure_future(regional_weather_data())
     loop.run_forever()
 except KeyboardInterrupt:
     pass
 finally:
     print("Closing Loop")
     loop.close()
+"""End of loop"""
